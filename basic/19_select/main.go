356287129baf68c7895e276b8b0e0f21c2683b86
cylantian <qazworkjava@yeah.net>
2025-04-25 22:28:10 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	basic/task1/main.go
    #	basic/task1/rob/rob.go
    #	basic/task1/singleNumber/singleNumber.go

--- basic/19_select/main.go
+++ basic/19_select/main.go
@@ -20,9 +20,17 @@ func main() {
 		case x := <-ch1:
 			fmt.Printf("receive %d from channel 1\n", x)
 		case y := <-ch2:
+
 			fmt.Printf("receive %d from channel 2\n", y)
 		case z := <-ch3:
+
 			fmt.Printf("receive %d from channel 3\n", z)
 		}
 	}
 }
+
+/*
+这是Go语言的通道接收操作符，箭头方向表示数据流动方向
+	<-ch 表示从通道 ch 接收数据
+	ch <- value 表示向通道 ch 发送数据
+*/
